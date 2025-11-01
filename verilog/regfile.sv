6727e45d779401bd9b2ac7554b24066ccf3b4a5a
Fah Yen <fahyen@umich.edu>
2025-11-01 17:08:46 -0400

    rewrote registerfile

--- verilog/regfile.sv
+++ verilog/regfile.sv
@@ -1,50 +1,44 @@
 `include "sys_defs.svh"
 
-module regfile (
+module reg_file (
     input logic clock,
-    input logic reset_n,
+    input logic reset,
 
-    input  PHYS_TAG [2*`N-1:0] read_idx,
-    output DATA     [2*`N-1:0] read_out,
+    input PHYS_TAG [2*`N-1:0] read_tags,
+    output DATA    [2*`N-1:0] read_outputs,
 
-    input logic    [`N-1:0] write_en,
-    input PHYS_TAG [`N-1:0] write_idx,
+    input logic    [`N-1:0] write_en, // At most `N inst completes
+    input PHYS_TAG [`N-1:0] write_tags,
     input DATA     [`N-1:0] write_data
 );
 
-    localparam int READ_PORTS = 2 * `N;
-    localparam int WRITE_PORTS = `N;
-
-    DATA mem[`PHYS_REG_SZ_R10K-1:0];
+    DATA [`PHYS_REG_SZ_R10K-1:0] register_file_entries, register_file_entries_next; // synthesis inference: packed array -> flip flops,  unpacked array -> RAM
 
     always_comb begin
-        for (int rp = 0; rp < READ_PORTS; rp++) begin
-            DATA val;
+        register_file_entries_next = register_file_entries;
 
-            if (read_idx[rp] == PHYS_TAG'(0)) val = '0;
-            else val = mem[read_idx[rp]];
-
-            for (int wp = 0; wp < WRITE_PORTS; wp++) begin
-                if (write_en[wp] && (write_idx[wp] != PHYS_TAG'(0)) && (write_idx[wp] == read_idx[rp])) begin
-                    val = write_data[wp];
-                end
+        for (int i = 0; i < 2 * `N; i++) begin
+            if (read_tags[i] == '0) begin // 0 register read
+                read_outputs[i] = '0;
+            end else if (read_tags[i] == write_tags[i]) begin // write forwarding
+                read_outputs[i] = write_data[i];
+            end else begin
+                read_outputs[i] = register_file_entries[read_tags[i]]; // normal read
             end
+        end
 
-            read_out[rp] = val;
+        for (int i = 0; i < `N; i++) begin
+            if (write_en[i]) begin
+                register_file_entries_next[write_tags[i]] = write_data[i];
+            end
         end
     end
 
-
-    always_ff @(posedge clock or negedge reset_n) begin
-        if (!reset_n) begin
-            for (int i = 0; i < `PHYS_REG_SZ_R10K; i++) mem[i] <= '0;
+    always_ff @(posedge clock) begin
+        if (reset) begin
+            register_file_entries <= '0;
         end else begin
-            // commit up to N writes; ignore writes to phys-0
-            for (int wp = 0; wp < WRITE_PORTS; wp++) begin  // variable wp here means "read port"
-                if (write_en[wp] && (write_idx[wp] != PHYS_TAG'(0))) mem[write_idx[wp]] <= write_data[wp];
-            end
-            // keep phys-0 hardwired to zero
-            mem[PHYS_TAG'(0)] <= '0;
+            register_file_entries <= register_file_entries_next;
         end
     end
 
