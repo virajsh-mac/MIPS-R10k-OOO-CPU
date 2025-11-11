cd4a2c90bc309dfa8289a3732479e88f23a141bf
mo0nbase <gpggit.difficult148@passmail.net>
2025-11-11 15:29:16 -0500

    cleanup
    Co-authored-by: Fah Yen <fahyen@umich.edu>

--- verilog/icache_subsystem.sv
+++ verilog/icache_subsystem.sv
@@ -21,53 +21,6 @@ module icache_subsystem (
 
 endmodule
 
-module victim_cache (
-    // lookup suite
-    input logic lookup_en,
-    input ADDR lookup_addr,
-
-    // Insert suite
-    input logic insert_en,
-    input ADDR insert_addr,
-    input cache_data insert_data,
-
-    // swap suite
-    input logic swap_en,
-    input ADDR swap_addr,
-    input cache_data swap_data,
-
-
-    // output
-    output logic vc_full,
-
-    output cache_data reinstate_data,
-    out ADDR reinstate_addr
-
-    output logic vc_hit,
-    // TODO: if hit, needs to output cache line back to icache
-)
-
-
-
-
-    // TODO finish victim cache
-    memDP #(
-        .WIDTH   ($BITS(MEM_BLOCK)),
-        .DEPTH   (4),      // victim cache only allowed 4 mem_blocks
-        .READ_PORTS(1),
-        .BYPASS_EN (0)
-    ) cache_bank[1:0] (
-        .clock(clock),
-        .reset(reset),
-        .re   ('1),
-        .raddr(),
-        .rdata(),
-        .we(),
-        .waddr(),
-        .wdata(write_data.cache_line)
-    );
-
-endmodule
 
 // ============================================================================
 // ICache (2-way banked, fully associative per bank)
@@ -226,5 +179,123 @@ module one_hot_to_index #(
     for (genvar i = 0; i < INPUT_WIDTH; i++) begin : gen_index_terms
         assign index = {INDEX_WIDTH{one_hot[i]}} & i;
     end
+
+endmodule
+
+module LFSR #(parameter NUM_BITS) (
+   input clock,
+   input reset,
+
+   input [NUM_BITS-1:0] seed_data,
+   output [NUM_BITS-1:0] data_out
+);
+
+    logic [NUM_BITS-1:0] LFSR;
+    logic                r_XNOR;
+
+    always @(posedge clock) begin
+        if (reset)
+           LFSR <= seed_data;
+        else
+           LFSR <= {LFSR[NUM_BITS-1:1], r_XNOR};
+    end
+
+    always @(*) begin
+        case (NUM_BITS)
+            3: begin
+                r_XNOR = LFSR[3] ^~ LFSR[2];
+            end
+            4: begin
+                r_XNOR = LFSR[4] ^~ LFSR[3];
+            end
+            5: begin
+                r_XNOR = LFSR[5] ^~ LFSR[3];
+            end
+            6: begin
+                r_XNOR = LFSR[6] ^~ LFSR[5];
+            end
+            7: begin
+                r_XNOR = LFSR[7] ^~ LFSR[6];
+            end
+            8: begin
+                r_XNOR = LFSR[8] ^~ LFSR[6] ^~ LFSR[5] ^~ LFSR[4];
+            end
+            9: begin
+                r_XNOR = LFSR[9] ^~ LFSR[5];
+            end
+            10: begin
+                r_XNOR = LFSR[10] ^~ LFSR[7];
+            end
+            11: begin
+                r_XNOR = LFSR[11] ^~ LFSR[9];
+            end
+            12: begin
+                r_XNOR = LFSR[12] ^~ LFSR[6] ^~ LFSR[4] ^~ LFSR[1];
+            end
+            13: begin
+                r_XNOR = LFSR[13] ^~ LFSR[4] ^~ LFSR[3] ^~ LFSR[1];
+            end
+            14: begin
+                r_XNOR = LFSR[14] ^~ LFSR[5] ^~ LFSR[3] ^~ LFSR[1];
+            end
+            15: begin
+                r_XNOR = LFSR[15] ^~ LFSR[14];
+            end
+            16: begin
+                r_XNOR = LFSR[16] ^~ LFSR[15] ^~ LFSR[13] ^~ LFSR[4];
+            end
+            17: begin
+                r_XNOR = LFSR[17] ^~ LFSR[14];
+            end
+            18: begin
+                r_XNOR = LFSR[18] ^~ LFSR[11];
+            end
+            19: begin
+                r_XNOR = LFSR[19] ^~ LFSR[6] ^~ LFSR[2] ^~ LFSR[1];
+            end
+            20: begin
+                r_XNOR = LFSR[20] ^~ LFSR[17];
+            end
+            21: begin
+                r_XNOR = LFSR[21] ^~ LFSR[19];
+            end
+            22: begin
+                r_XNOR = LFSR[22] ^~ LFSR[21];
+            end
+            23: begin
+                r_XNOR = LFSR[23] ^~ LFSR[18];
+            end
+            24: begin
+                r_XNOR = LFSR[24] ^~ LFSR[23] ^~ LFSR[22] ^~ LFSR[17];
+            end
+            25: begin
+                r_XNOR = LFSR[25] ^~ LFSR[22];
+            end
+            26: begin
+                r_XNOR = LFSR[26] ^~ LFSR[6] ^~ LFSR[2] ^~ LFSR[1];
+            end
+            27: begin
+                r_XNOR = LFSR[27] ^~ LFSR[5] ^~ LFSR[2] ^~ LFSR[1];
+            end
+            28: begin
+                r_XNOR = LFSR[28] ^~ LFSR[25];
+            end
+            29: begin
+                r_XNOR = LFSR[29] ^~ LFSR[27];
+            end
+            30: begin
+                r_XNOR = LFSR[30] ^~ LFSR[6] ^~ LFSR[4] ^~ LFSR[1];
+            end
+            31: begin
+                r_XNOR = LFSR[31] ^~ LFSR[28];
+            end
+            32: begin
+                r_XNOR = LFSR[32] ^~ LFSR[22] ^~ LFSR[2] ^~ LFSR[1];
+            end
+
+        endcase
+    end
+
+    assign data_out = LFSR;
 
 endmodule
